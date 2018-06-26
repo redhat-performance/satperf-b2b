6bd1f7b19495b95e19f130c2e2a581805f12c129
Saurabh Badhwar <sbadhwar@redhat.com>
2018-06-26 19:41:00 +0530

    Enable the migration of the database
    Signed-off-by: Saurabh Badhwar <sbadhwar@redhat.com>

--- b2btool/__init__.py
+++ b2btool/__init__.py
@@ -5,4 +5,7 @@ Description: Marks the place for application entrypoint.
 Date: 26/06/2018
 Author: Saurabh Badhwar
 """
-from .app import application, db
+from b2btool.app import application, db
+from b2btool.models import Query, Build
+
+db.create_all()
