a370de812b8c54575682ac27af7415ed8fc9baa6
Saurabh Badhwar <sbadhwar@redhat.com>
2018-06-28 16:48:14 +0530

    Add metric retrieval methods to graphite helper
    Signed-off-by: Saurabh Badhwar <sbadhwar@redhat.com>

--- b2btool/helpers/graphite.py
+++ b2btool/helpers/graphite.py
@@ -8,7 +8,7 @@ Date: 27/06/2018
 Author: Saurabh Badhwar
 """
 import requests
-
+import json
 
 class Graphite(object):
     """Graphite API abstraction layer.
@@ -28,11 +28,60 @@ class Graphite(object):
         """
         self.graphite_hostname = hostname
         self.graphite_port = port
-        self.query_url = "{}:{}".format(self.graphite_hostname,self.graphite_port)
+        self.query_url = "{}:{}".format(self.graphite_hostname, 
+                                        self.graphite_port)
         if self.__validate_group(group):
             self.lookup_group = group
         else:
-            raise Exception("Unable to find the required data group in graphite")
+            raise Exception("Unable to find the required data group in \
+                            graphite")
+
+    def get_metrics(self, hostname):
+        """Get the metrics from the graphite server.
+
+        The method provides functionality for retrieving the metrics from
+        the graphite server which are present under a provided hostname.
+
+        Keyword arguments:
+        hostname -- The hostname for which the metrics needs to be retrieved
+
+        Returns:
+            List
+
+        """
+        self.query_url = "{}:{}/metrics/find?query={}.{}.*".format(self.graphite_hostname, self.graphite_port, self.lookup_group, hostname)
+        req = requests.get(self.query_url)
+        response_data = json.loads(req.text)
+        metrics = []
+        for metric in response_data:
+            metrics.append(metric['id'])
+        return metrics
+
+    def get_measurable_metrics(self, path):
+        """Get the measurable metric paths.
+
+        The method provides a mechanism for retrieving the measurable
+        metric paths from the provided expandable path.
+
+        Keyword arguments:
+        path -- The path from which measurable metrics need to be gathered
+
+        Returns:
+            List
+        
+        """
+        self.query_url = "{}:{}/metrics/find?query={}.*".format(self.graphite_hostname, self.graphite_port, path)
+        req = requests.get(self.query_url)
+        response_data = json.loads(req.text)
+        metrics = []
+        for metric in response_data:
+            if metric['lead'] == 1:
+                metrics.append(metric['id'])
+            else:
+                tmp_metrics = self.get_measurable_metrics(metric['id'])
+                for tmp_metric in tmp_metrics:
+                    metrics.append(tmp_metric)
+        return metrics
 
     def __validate_group(self, group):
         """Validate the provided group.
@@ -45,6 +94,7 @@ class Graphite(object):
 
         Returns:
             bool
+
         """
         query = self.query_url + "/metrics/find?query={}.*".format(group)
         resp = requests.get(query)
