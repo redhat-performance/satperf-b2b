4939c35cb80c7942720cd95c1e69c46204e7a840
Saurabh Badhwar <sbadhwar@redhat.com>
2018-06-29 10:00:04 +0530

    Expose more routes
    Signed-off-by: Saurabh Badhwar <sbadhwar@redhat.com>

--- b2btool/__init__.py
+++ b2btool/__init__.py
@@ -5,9 +5,15 @@ Description: Marks the place for application entrypoint.
 Date: 26/06/2018
 Author: Saurabh Badhwar
 """
-from b2btool.app import application, db
+from b2btool.app import application, db, graphite
 from b2btool.models import Query, Build
-from b2btool.views import NewQuery
+from b2btool.views import NewQuery, NewBuild, BuildTags, BuildHosts, BuildBenchmarks, BuildMetrics, BuildLeafMetrics
 
 application.add_url_rule('/newquery', view_func=NewQuery.as_view('new_query'))
+application.add_url_rule('/newbuild', view_func=NewBuild.as_view('new_build'))
+application.add_url_rule('/tags', view_func=BuildTags.as_view('build_tags'))
+application.add_url_rule('/hosts', view_func=BuildHosts.as_view('build_hosts'))
+application.add_url_rule('/benchmarks', view_func=BuildBenchmarks.as_view('build_benchmarks'))
+application.add_url_rule('/build_metrics', view_func=BuildMetrics.as_view('build_metrics'))
+application.add_url_rule('/leaf_metrics', view_func=BuildLeafMetrics.as_view('leaf_metrics'))
 db.create_all()
